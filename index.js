e44370a8d7ff08931c95f0ba76c7307de0814c07
TreeWhispererA <139618051+TreeWhispererA@users.noreply.github.com>
2023-07-24 17:56:50 +0900

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.log('hello world');
